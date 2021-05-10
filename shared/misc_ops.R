1f8e8edf713b4cae128d11231727869afa7f853f
Julian Zhou <julianqz@users.noreply.github.com>
2021-05-10 11:20:23 -0700

    renamed `db` to `df`

--- shared/misc_ops.R
+++ shared/misc_ops.R
@@ -2,21 +2,21 @@
 
 #' Select rows with specific values in specific columns
 #'
-#' @param  db       A `data.frame`.
+#' @param  df       A `data.frame`.
 #' @param  col_vec  A `vector` containing column names.
 #' @param  val_lst  A `list` containing column values.
 #' 
-#' @return  A `boolean` vector matching `nrow(db)`.
+#' @return  A `boolean` vector matching `nrow(df)`.
 #' 
 #' @details There must be a one-to-one match between the entries in `col_vec` and
 #'          the entries in `val_lst`. 
 #'          Each entry in `col_vec` must be a single string.
 #'          Each entry in `val_lst` must be one or more values.
 #' 
-db_select = function(db, col_vec, val_lst) {
+df_select = function(df, col_vec, val_lst) {
    # checks
     stopifnot( length(col_vec) == length(val_lst) )
-    stopifnot( all( col_vec %in% colnames(db) ) )
+    stopifnot( all( col_vec %in% colnames(df) ) )
     
     bool_lst = sapply(1:length(col_vec), 
                       function(i) {
@@ -24,12 +24,12 @@ db_select = function(db, col_vec, val_lst) {
                           cur_col = col_vec[i]
                           # one or more values in that column
                           cur_val = val_lst[[i]]
-                          cur_bool = db[[cur_col]] %in% cur_val
+                          cur_bool = df[[cur_col]] %in% cur_val
                           return(cur_bool)
                       },
                       USE.NAMES=F, simplify=F)
     
-    # rows match rows in db
+    # rows match rows in df
     # cols match col_vec
     bool_mtx = do.call(cbind, bool_lst)
     
