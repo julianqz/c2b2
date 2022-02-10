378d984b47a8d7b93d5dde7b00ba7c3f476deea8
Julian Zhou <julianqz@users.noreply.github.com>
2022-02-10 11:34:19 -0500

    added doc

--- shared/misc_barplot.R
+++ shared/misc_barplot.R
@@ -1,10 +1,45 @@
-#### percentage barplot ####
 
-# TODO: add doc
-
+#' Enhanced barplot
+#' 
+#' @param  tab         Input counts. Either as a 1-D vector, or a 2-D matrix.
+#'                     See details. 
+#' @param  vec_ylim    Y-axis range limits. Passed on to `barplot`.
+#' @param  vec_labels  Text labels on top of the bars. Usually counts of sort.
+#'                     Optional. Default is `NULL`.
+#'                     To specify, supply a character vector whose length matches 
+#'                     the length (if 1-D vector) or nrow (if 2-D matrix) of `tab`.
+#' @param  las         Orientation of labels. Passed on to `barplot`.
+#'                     Default is `2`.
+#' @param  ylab        Y-axis label. Passed on to `barplot`. Default is `""`.
+#' @param  cex.lab     Size of labels. Passed on to `barplot`. Default is `1.25`.
+#' @param  col_bar     Color of bars. Passed on to `barplot`.
+#'                     If `tab` is a 1-D vector, entries in `col_bar` should match
+#'                     entries in `tab`. If `tab` is a 2-D matrix, entries in 
+#'                     `col_bar` should match the columns of `tab`.
+#' @param  col_border  Color of border of bars. Passed on to `barplot`. 
+#'                     Default is `NA` (no border).
+#' 
+#' @return  A barplot with proper placement of x-axis labels. 
+#' 
+#' @details If `tab` is a 1-D vector, the plot rendered is identical to that 
+#'          rendered by `graphics::barplot(tab)` (other than aesthetic details). 
+#'          
+#'          If `tab` is a 2-D matrix, the plot rendered is identical to that
+#'          rendered by `graphics::barplot(t(tab))` (other than aesthetic details).
+#'          The rows of `tab` correspond to columns in the barplot. Rownames show
+#'          up as x-axis labels.
+#'          The columns of `tab` correspond to segments in each column, and 
+#'          are colored according to `col_bar`.
+#'          
+#' @example tab=matrix(c(7.7, 8.1, 7, 72.1, 5.1, 12.6, 9.1, 2, 70.8, 5.5), nrow=2, byrow=T)
+#'          colnames(tab) = paste0("Ig", c("A","D","E","G","M"))
+#'          rownames(tab) = c("donor_1", "donor_2")
+#'          barplot_2(tab, vec_ylim=c(0,105), las=1, col_bar=1:5, ylab="%")
+#'          
 barplot_2 = function(tab, vec_ylim, vec_labels=NULL, 
                      las=2, ylab="", cex.lab=1.25, 
                      col_bar, col_border=NA) {
+    
     if (is.null(dim(tab))) {
         len = length(tab)
     } else {
@@ -14,6 +49,7 @@ barplot_2 = function(tab, vec_ylim, vec_labels=NULL,
     if (is.null(vec_labels)) {
         vec_labels = rep("", len)
     }
+    stopifnot(length(vec_labels)==len)
     
     bar_widths = rep(1, len)
     bar_spaces = rep(0.2, len)
@@ -34,3 +70,4 @@ barplot_2 = function(tab, vec_ylim, vec_labels=NULL,
             las=las, ylab=ylab, cex.lab=cex.lab)
     text(x=bar_se_x, y=vec_ylim[2]*0.98, labels=vec_labels)
 }
+
