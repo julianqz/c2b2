480d81914d2674619bedde21aa27d052396935f6
Julian Zhou <julianqz@users.noreply.github.com>
2022-02-10 10:53:44 -0500

    added doc

--- shared/bcr_vj.R
+++ shared/bcr_vj.R
@@ -1,26 +1,49 @@
-RUN=F
-if (RUN) {
-    vec_v = db[["germline_v_call"]]
-    vec_j = db[["germline_j_call"]]
-    vec_subj = db[["donor"]]
-    anno_separator=","
-    
-    tmp = tabulate_vj(db[["germline_v_call"]], db[["germline_j_call"]],
-                      db[["donor"]], ",")
-    
-    str(tmp[["common"]])
-    str(tmp[["subj_specific"]])
-    
-}
-
-# tip: if data for different subjects are stored in separate
-#      data.frames, concat to create the required input vectors
-
+#' Tabulate V-J gene usage
+#' 
+#' @param   vec_v          Character vector containing V gene/allele annotations.
+#'                         Vector length should match that of `vec_j` and `vec_subj`. 
+#' @param   vec_j          Character vector containing J gene/allele annotations.
+#'                         Vector length should match that of `vec_v` and `vec_subj`.
+#' @param   vec_subj       Character vector containing subject labels.
+#'                         Vector length should match that of `vec_v` and `vec_j`.
+#' @param   anno_separator Character delimiting ambiguous annotations. Passed on
+#'                         to `sep` of `alakazam::getGene`.
+#' 
+#' @return  A nested list containing `common` and `subj_specific`, each of which
+#'          is itself a list of `data.frame`. 
+#'          
+#'          The number of `data.frame`s matches the number of unique subjects 
+#'          in `vec_subj`. Each `data.frame` contains columns `subj`, `v`, `j`, 
+#'          and `count`.
+#'          
+#'          In `common`, each `data.frame` contains the same number of rows 
+#'          (i.e. same total set of V-J combinations) for all subjects. In 
+#'          other words, the V-J combinations being counted in `common` are the
+#'          union of V-J combinations found in each subject. 
+#'          
+#'          In `subj_specific`, each `data.frame` contains a number of rows 
+#'          specific to the subject being represented. 
+#'          
+#'          Both `common` and `subj_specific` are returned so as to provide  
+#'          convenience when visualizing -- `common` allows heatmaps with common
+#'          rows and columns to be drawn for all subjects, whereas `subj_specific`
+#'          allows heatmaps with unique rows and columns to be drawn for each subject.
+#' 
+#' @details Tip: if data for different subjects are stored in separate `data.frame`s, 
+#'          concatenate to create the required input vectors.
+#' 
+#' @example tabulate_vj(vec_v=db[["germline_v_call"]], 
+#'                      vec_j=db[["germline_j_call"]],
+#'                      vec_subj=db[["donor"]], ",")         
+#'                      
 tabulate_vj = function(vec_v, vec_j, vec_subj, 
-                      anno_separator=",") {
+                       anno_separator=",") {
     
     # getGene
     require(alakazam) 
+    
+    stopifnot(length(vec_v)==length(vec_j))
+    stopifnot(length(vec_v)==length(vec_subj))
     
     # unique entries
     vec_v_uniq = unique(vec_v)
@@ -29,12 +52,18 @@ tabulate_vj = function(vec_v, vec_j, vec_subj,
     
     # extract v/j gene for unique entries
     # note that vec_[vj]_uniq_getGene could still contain duplicates
+    
+    # important: do not set strip_d to True (which is the default)
+    # getGene assumes any allele with a "D" in name is a duplicate, 
+    #   which is decisively not the case 
+    #   (e.g. IGHV3-43 != iGHV3-43D nucleotide-wise)
+    
     vec_v_uniq_getGene = getGene(segment_call=vec_v_uniq,
-                              first=F, collapse=T, strip_d=F, omit_nl=F, 
-                              sep=anno_separator)
+                                 first=F, collapse=T, strip_d=F, omit_nl=F, 
+                                 sep=anno_separator)
     vec_j_uniq_getGene = getGene(segment_call=vec_j_uniq,
-                              first=F, collapse=T, strip_d=F, omit_nl=F, 
-                              sep=anno_separator)
+                                 first=F, collapse=T, strip_d=F, omit_nl=F, 
+                                 sep=anno_separator)
     
     # map back v/j gene to all entries
     # wrt vec_[vj]_uniq
@@ -163,3 +192,4 @@ tabulate_vj = function(vec_v, vec_j, vec_subj,
     #return(list("common"=df_common, "subj_specific"=df_subj))
     return(list("common"=lst_df_common, "subj_specific"=lst_df_subj))
 }
+
